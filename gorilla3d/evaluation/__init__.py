db3b332f8edbceffb53de5bff2da6aec5bc9f705
lzhnb <liang13268932888@outlook.com>
2020-12-02 23:58:21 +0800

    finish scannet evaluator initially

--- gorilla3d/evaluation/__init__.py
+++ gorilla3d/evaluation/__init__.py
@@ -2,6 +2,6 @@
 from .metric import (evaluate, read_gt,
                      assign_instances_for_scan, evaluate_matches, compute_averages, print_results)
 
-from .scannet_evaluator import ScanNetSemanticEvaluator
+from .scannet_evaluator import (ScanNetSemanticEvaluator, ScanNetInstanceEvaluator, ScanNetEvaluator)
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
--- gorilla3d/evaluation/scannet_evaluator.py
+++ gorilla3d/evaluation/scannet_evaluator.py
@@ -5,7 +5,9 @@ import torch
 import numpy as np
 
 from gorilla.evaluation import DatasetEvaluator, DatasetEvaluators
-from .metric import read_gt, evaluate
+from .metric import (read_gt, evaluate, assign_instances_for_scan,
+                     evaluate_matches, compute_averages, print_results)
+
 
 class ScanNetSemanticEvaluator(DatasetEvaluator):
     """
@@ -13,20 +15,17 @@ class ScanNetSemanticEvaluator(DatasetEvaluator):
     """
 
     def __init__(
-        self, dataset_root, num_classes=None, ignore_label=None, logger=None
+        self, dataset_root, logger=None
     ):
         """
         Args:
             num_classes, ignore_label: deprecated argument
         """
         self._dataset_root = dataset_root
-        self._num_classes = num_classes
-        self._ignore_label = ignore_label if ignore_label is not None else ignore_label
         self.logger = logger
         self.reset()
 
     def reset(self):
-        self._conf_matrix = np.zeros((self._num_classes + 1, self._num_classes + 1), dtype=np.int64)
         self._predictions = {}
         self._gt = {}
 
@@ -41,11 +40,11 @@ class ScanNetSemanticEvaluator(DatasetEvaluator):
         """
         for input, output in zip(inputs, outputs):
             scene_name = input["scene_name"]
-            semantic_gt = read_gt(osp.join(self._dataset_root, scene_name), scene_name)
-            semantic_pred = output.cpu().numpy()
+            semantic_gt = read_gt(
+                osp.join(self._dataset_root, scene_name), scene_name)
+            semantic_pred = output["semantic_pred"].cpu().numpy()
             self._gt[scene_name] = semantic_gt
             self._predictions[scene_name] = semantic_pred
-
 
     def evaluate(self):
         """
@@ -62,5 +61,62 @@ class ScanNetSemanticEvaluator(DatasetEvaluator):
             matches[scene_name]["semantic_pred"] = self._predictions[scene_name]
 
         evaluate(matches, self.logger)
+
+
+class ScanNetInstanceEvaluator(DatasetEvaluator):
+    """
+    Evaluate semantic segmentation metrics.
+    """
+
+    def __init__(
+        self, dataset_root, logger=None
+    ):
+        """
+        Args:
+            num_classes, ignore_label: deprecated argument
+        """
+        self._dataset_root = dataset_root
+        self.logger = logger
+        self.reset()
+
+    def reset(self):
+        self._predictions = {}
+        self._gt = {}
+
+    def process(self, inputs, outputs):
+        """
+        Args:
+            inputs: the inputs to a model.
+                It is a list of dicts.
+            outputs: the outputs of a model. It is either list of semantic segmentation predictions
+                or list of dicts with key "sem_seg" that contains semantic
+                segmentation prediction in the same format.
+        """
+        for input, output in zip(inputs, outputs):
+            scene_name = input["scene_name"]
+            gt_file = osp.join(self._dataset_root, scene_name + ".txt")
+            gt2pred, pred2gt = assign_instances_for_scan(scene_name, output, gt_file)
+            self._gt[scene_name] = gt2pred
+            self._predictions[scene_name] = pred2gt
+
+    def evaluate(self):
+        """
+        Evaluates standard semantic segmentation metrics (http://cocodataset.org/#stuff-eval):
+        * Mean intersection-over-union averaged across classes (mIoU)
+        * Frequency Weighted IoU (fwIoU)
+        * Mean pixel accuracy averaged across classes (mACC)
+        * Pixel Accuracy (pACC)
+        """
+        matches = {}
+        for scene_name in self._gt.keys():
+            matches[scene_name] = {}
+            matches[scene_name]["gt"] = self._gt[scene_name]
+            matches[scene_name]["pred"] = self._predictions[scene_name]
+        
+        ap_scores = evaluate_matches(matches)
+        avgs = compute_averages(ap_scores)
+        print_results(avgs, self.logger)
+
+ScanNetEvaluator = DatasetEvaluators([ScanNetSemanticEvaluator, ScanNetInstanceEvaluator])
 
 
