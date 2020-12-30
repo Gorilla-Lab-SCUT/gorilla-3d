dd4093612c84a7e47dac18d6203d284ae1b51da0
lzhnb <liang13268932888@outlook.com>
2020-12-30 12:28:49 +0800

    fix save pc

--- gorilla3d/utils/pc.py
+++ gorilla3d/utils/pc.py
@@ -119,21 +119,23 @@ def save_pc(points: Union[np.ndarray, torch.Tensor],
             colors = colors.cpu().numpy()
     try:
         import open3d as o3d
-        if colors.max() > 1:
-            colors = colors / 255
         pc = o3d.geometry.PointCloud()
         pc.points = o3d.utility.Vector3dVector(points)
-        if colors is not None:
+        if colors is not None and colors.max() > 1:
+            colors = colors / 255
             pc.colors = o3d.utility.Vector3dVector(colors)
         o3d.io.write_point_cloud(filename, pc)
     except:
-        if colors.max() < 1:
+        if colors is not None and colors.max() < 1:
             colors = (colors * 255).astype(np.int32)
-        vc = np.concatenate([points, colors], axis=1)
-        vc = [tuple(vc[i]) for i in range(vc.shape[0])]
-        vc = np.array(vc, dtype=[("x", "f4"), ("y", "f4"), ("z", "f4"),
-                                 ("r", "i4"), ("g", "i4"), ("b", "i4")])
+            vc = np.concatenate([points, colors], axis=1)
+            vc = [tuple(vc[i]) for i in range(vc.shape[0])]
+            vc = np.array(vc, dtype=[("x", "f4"), ("y", "f4"), ("z", "f4"),
+                                    ("r", "i4"), ("g", "i4"), ("b", "i4")])
+        else:
+            vc = points
+            vc = [tuple(vc[i]) for i in range(vc.shape[0])]
+            vc = np.array(vc, dtype=[("x", "f4"), ("y", "f4"), ("z", "f4")])
         el = PlyElement.describe(vc, "vertex")
         PlyData([el]).write(filename)
-
 
