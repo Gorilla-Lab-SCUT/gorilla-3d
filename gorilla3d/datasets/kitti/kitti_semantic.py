77a46666156044168356d0a624375637910f06d7
lzhnb <liang13268932888@outlook.com>
2021-04-30 05:25:11 +0000

    support the range input for kitti_semantic dataset(need to merge together)

--- gorilla3d/datasets/kitti/kitti_semantic.py
+++ gorilla3d/datasets/kitti/kitti_semantic.py
@@ -21,6 +21,7 @@ class KittiSem(Dataset):
                  label_mapping: str="semantic-kitti.yaml",
                  return_ref: bool=False,
                  return_test: bool=False,
+                 project: bool=False,
                  transform_cfg: Dict=dict(
                     rotate_aug=False,
                     flip_aug=False,
@@ -56,9 +57,20 @@ class KittiSem(Dataset):
 
         self.logger.info("Using {} scans from sequences {}".format(len(self.data_files), self.sequences))
 
+        # initialize point cloud transformer
         self.pc_transformer = PointCloudTransfromer(**transform_cfg)
         processer_caller = globals()[grid_cfg["type"]]
+        # initialize voxelizer
         self.processer = processer_caller(**grid_cfg)
+
+        # projection(range view)
+        self.project = project
+        if self.project:
+            self.max_points = kwargs.get("max_points", 150000)
+            self.scan = SemLaserScan(**kwargs["sensor"])
+            self.color_map = self.semkittiyaml["color_map"]
+            self.sensor_img_means = np.array(kwargs.get("img_means", [12.12, 10.88, 0.23, -1.04, 0.21]), dtype=np.float32)
+            self.sensor_img_stds = np.array(kwargs.get("img_stds", [12.32, 11.47, 6.91, -0.86, 0.16]), dtype=np.float32)
 
     def __len__(self):
         "Denotes the total number of samples"
@@ -85,6 +97,40 @@ class KittiSem(Dataset):
             return_fea = np.concatenate((processed_xyz, sig[..., np.newaxis]), axis=1)
 
         data_tuple = (voxel_position, processed_label, grid_ind, labels, xyz, return_fea)
+
+        if self.project:
+            # reset scan
+            self.scan.reset()
+
+            # read points and labels
+            self.scan.set_points(raw_data[:, :3], raw_data[:, 3])
+            self.scan.set_label(annotated_data)
+            # NOTE: the label has been mapped, do not need to map again
+            # self.scan.sem_label_map(self.label_mapper) # map labels
+
+            # get un-projected paramters(origin input)
+            npoint = self.scan.points.shape[0]
+            num_pad = self.max_points - npoint
+
+            # get projected paramters
+            proj_range = self.scan.proj_range.copy() # [H, W]
+            proj_xyz = self.scan.proj_xyz.copy() # [H, W, 3]
+            proj_remission = self.scan.proj_remission.copy() # [H, W]
+            proj_mask = self.scan.proj_mask.copy() # [H, W]
+            proj_label = self.scan.proj_sem_label.copy() # [H, W]
+            proj_label = proj_label * proj_mask # [H, W]
+            # model input
+            proj = np.concatenate([proj_range[None, :, :].copy(),
+                                   proj_xyz.transpose(2, 0, 1).copy(),
+                                   proj_remission[None, :, :].copy()]) # [5, H, W]
+            proj = (proj - self.sensor_img_means[:, None, None]) / self.sensor_img_stds[:, None, None] # [5, H, W]
+            proj = proj * proj_mask.astype(np.float32)
+
+            # get the projection x and y ids(in pixel coordinates)
+            proj_x = np.pad(self.scan.proj_x, (0, num_pad), constant_values=(-1.0, -1.0)).astype(np.int64) # [max_points]
+            proj_y = np.pad(self.scan.proj_y, (0, num_pad), constant_values=(-1.0, -1.0)).astype(np.int64) # [max_points]
+            data_tuple += (proj, proj_label, proj_x, proj_y, npoint)
+
         return data_tuple
 
     @property
@@ -102,6 +148,15 @@ class KittiSem(Dataset):
         point_labels = []
         point_xyzs = []
         point_features = []
+        # TODO: ugly
+        proj_flag = len(batch[0]) > 6
+        if proj_flag:
+            projs = []
+            proj_labels = []
+            proj_xs = []
+            proj_ys = []
+            npoints = []
+
         for i, b in enumerate(batch):
             voxel_centers.append(torch.from_numpy(b[0]).float())
             voxel_labels.append(torch.from_numpy(b[1]).long())
@@ -109,6 +164,13 @@ class KittiSem(Dataset):
             point_labels.append(torch.from_numpy(b[3]))
             point_xyzs.append(torch.cat([torch.FloatTensor(b[4].shape[0], 1).fill_(i), torch.from_numpy(b[4])], 1))
             point_features.append(torch.from_numpy(b[5]).float())
+            if proj_flag:
+                projs.append(torch.from_numpy(b[6]).float())
+                proj_labels.append(torch.from_numpy(b[7]).float())
+                proj_xs.append(torch.from_numpy(b[8]).long())
+                proj_ys.append(torch.from_numpy(b[9]).long())
+                npoints.append(torch.Tensor(b[10]).long())
+
         
         voxel_centers = torch.stack(voxel_centers) # [B, H, W, D, 3]
         voxel_labels = torch.stack(voxel_labels) # [B, H, W, D]
@@ -117,7 +179,7 @@ class KittiSem(Dataset):
         point_xyzs = torch.cat(point_xyzs, 0) # [N, 3]
         point_features = torch.cat(point_features, 0) # [N, C]
 
-        return {
+        data =  {
             "voxel_centers": voxel_centers,
             "voxel_labels": voxel_labels,
             "grid_inds": grid_inds,
@@ -125,6 +187,22 @@ class KittiSem(Dataset):
             "point_xyzs": point_xyzs,
             "point_features": point_features
         }
+
+        if proj_flag:
+            projs = torch.stack(projs) # [B, 5, H, W]
+            proj_labels = torch.stack(proj_labels) # [B, H, W]
+            proj_xs = torch.stack(proj_xs) # [B, max_point]
+            proj_ys = torch.stack(proj_ys) # [B, max_point]
+            npoints = torch.cat(npoints) # [B]
+            data.update({
+                "projs": projs,
+                "proj_labels": proj_labels,
+                "proj_xs": proj_xs,
+                "proj_ys": proj_ys,
+                "npoints": npoints,
+            })
+
+        return data
 
 
 
@@ -162,6 +240,7 @@ class KittiSemRV(KittiSem):
 
     def collate_fn(self, batch):
         return torch.utils.data._utils.collate.default_collate(batch)
+
 
     def __getitem__(self, index):
         gt_flag = self.task != "test"
